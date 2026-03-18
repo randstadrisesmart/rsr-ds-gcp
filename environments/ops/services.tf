49988698a4a8932a97183470f861c4bef9dc4ad7
wayne.kenney <wayne.kenney@randstadsourceright.com>
2026-03-18 09:54:40 -0700

    Add write tables as once-sync to bootstrap PRD schema

--- environments/ops/services.tf
+++ environments/ops/services.tf
@@ -66,6 +66,10 @@ locals {
       build_group = "ollama"
       sync_tables = [
         { dataset_name = "rasco_taxonomy_fixes_US", table_name = "rasco_taxonomy", sync_frequency = "daily", region = "us-east1" },
+        { dataset_name = "rasco_taxonomy_fixes_US", table_name = "job_title_input", sync_frequency = "once", region = "us-east1" },
+        { dataset_name = "rasco_taxonomy_fixes_US", table_name = "job_title_results", sync_frequency = "once", region = "us-east1" },
+        { dataset_name = "rasco_taxonomy_fixes_US", table_name = "processing_jobs", sync_frequency = "once", region = "us-east1" },
+        { dataset_name = "rasco_taxonomy_fixes_US", table_name = "rasco_fixer_output_integration", sync_frequency = "once", region = "us-east1" },
       ]
     }
   }
